e67b6a9ef38b24f0eaf706087700a55032518aa7
Karan Trivedi <trivedikaran13@gmail.com>
2019-04-21 20:38:41 +0530

    First Commit....

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Foood Mantra';
+  currentYear: number = new Date().getFullYear();
+}
